1eb65c12c36f1b000316922462b20fc345ecfdaf
unknown <camila.mata14@gmail.com>
2022-11-09 19:43:50 -0300

    commit connection DB

--- src/app.js
+++ src/app.js
@@ -6,4 +6,12 @@ app.use(cors());
 
 app.use(express.json());
 
+require("dotenv-safe").config();
+
+const db = require("./database/mongoDBconfig");
+db.connect();
+
+
+module.exports = app;
+
 // const clientRoute = require("./routes")
