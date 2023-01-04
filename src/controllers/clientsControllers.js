2b0905a9ba052dfa21e35589bce541e84dca84a8
unknown <camila.mata14@gmail.com>
2023-01-03 21:34:17 -0300

    :sparkles: search client by name

--- src/controllers/clientsControllers.js
+++ src/controllers/clientsControllers.js
@@ -1,15 +1,15 @@
 const ObjectId = require("mongodb").ObjectId;
 const clientModel = require("../models/clientsSchema");
 
-const register = async(req,res) => {
+const register = async (req, res) => {
     try {
         if (!req.body.name || !req.body.age || !req.body.weekDays) {
             res.status(406).send({
                 "message": "You can't register an Athlete without these infos :("
             })
         }
-        let {name, socialName, username, age, adress, phone, weekDays} = req.body;
-        let newClient = await clientModel.create({name, socialName,username, age, adress, phone, weekDays});    
+        let { name, socialName, username, age, adress, phone, weekDays } = req.body;
+        let newClient = await clientModel.create({ name, socialName, username, age, adress, phone, weekDays });
         let registeredClient = await newClient.save();
 
         if (registeredClient) {
@@ -19,10 +19,31 @@ const register = async(req,res) => {
             })
         }
     }
-    catch(error) {}
+    catch (error) { }
 };
+
+const getByName = async (req, res) => {
+    try {
+        let name = req.body.name
+        let foundClient = await clientModel.find({name: {$regex: name, $options: "ix"}})
+        
+        if(!foundClient) {
+            throw new Error ("No athletes with that name.")    
+        }
+        res.status(200).json({
+            "We found these results:": foundClient
+        })    
+    }
+    catch (error) {}
+}
+
+
+
+
+
 
 
 module.exports = {
-    register
+    register,
+    getByName
 }
--- src/routes/clientsRoutes.js
+++ src/routes/clientsRoutes.js
@@ -3,6 +3,7 @@ let controller = require("../controllers/clientsControllers");
 let router = express.Router();
 
 router.post("/registerClient", controller.register);
+router.get("/searchClient", controller.getByName);
 
 
 
